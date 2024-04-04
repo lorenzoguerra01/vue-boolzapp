34e72f491285ed218671205eacf53c31d2fb9004
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-04-04 17:53:39 +0200

    add milestone 3

--- js/script.js
+++ js/script.js
@@ -1,20 +1,44 @@
-import {isRanDOMImage} from './utility.js';
-import {contacts} from './data.js';
+import { isRanDOMImage } from './utility.js';
+import { contacts } from './data.js';
 
-const {createApp} = Vue;
+const { createApp } = Vue;
 createApp({
-    data(){
-        return{
+    data() {
+        return {
             contacts,
-            activeContactId : 1
+            activeContactId: 1,
+            chatText: ''
         }
     },
-    methods :{
+    methods: {
         isRanDOMImage,
         activeContact(id) {
             this.activeContactId = id;
             console.log(this.activeContactId);
+        },
+        enterText() {
+            let newText = {
+                date: new Date().toLocaleString(),
+                message: this.chatText,
+                status: 'sent'
+            }
+            contacts[this.activeContactId - 1].messages.push(newText);
+            console.log(newText);
+            console.log(contacts[this.activeContactId - 1].messages);
+            this.$forceUpdate();
+            setTimeout(() => {
+                let newText = {
+                    date: new Date().toLocaleString(),
+                    message: 'Ok',
+                    status: 'received'
+                }
+                contacts[this.activeContactId - 1].messages.push(newText);
+                this.$forceUpdate();
+            }, 1000)
         }
+    },
+    computed: {
+
     },
     mounted() {
     }
