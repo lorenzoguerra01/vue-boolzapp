e333c12e871ca6ef6a33f4364d55327d759e8b01
lorenzoguerra01 <guerra.lo11-lg@gmail.com>
2024-04-05 22:35:35 +0200

    add miklestone 5 (fixed)

--- js/script.js
+++ js/script.js
@@ -20,11 +20,11 @@ createApp({
             console.log(this.activeContactId);
         },
         getTimeFromDate(dateString) {
-            let date = new Date(dateString);
-            let hours = date.getHours();
-            let minutes = date.getMinutes().toString().padStart(2, '0');
+            const dateTime = dt.fromFormat(dateString, 'dd/MM/yyyy HH:mm:ss');
+            const hours = dateTime.hour;
+            const minutes = dateTime.minute.toString().padStart(2, '0');
             return `${hours}:${minutes}`;
-        },
+            },
         enterText() {
             if (this.chatText !== '') {
                 let newText = {
@@ -49,18 +49,34 @@ createApp({
         deleteText(i) {
             this.contacts[this.activeContactId - 1].messages.splice(i, 1);
         },
-        lastText() {
-            return this.contacts[this.activeContactId - 1].messages.length - 1;
+        getLastMessage(contact) {
+            const lastMessage = contact.messages[contact.messages.length - 1];
+            return lastMessage ? lastMessage.message : 'Nessun messaggio';
         },
-        lastText2(id) {
-            return this.contacts[id - 1].messages.length - 1;
+        getLastDate(contact) {
+            const lastMessage = contact.messages[contact.messages.length - 1];
+            return lastMessage ? this.getTimeFromDate(lastMessage.date) : 'N/A';
+        },
+        truncateMessage(message) {
+            const maxLength = 35;
+            return message.length > maxLength ? `${message.slice(0, maxLength)}...` : message;
         }
+    
 
     },
     computed: {
+        computedContact() {
+            return this.contacts.find((contact) => contact.id === this.activeContactId)
+        },
         searchedContacts() {
             let searchText = this.searchText.toLowerCase();
             return this.contacts.filter(contact => contact.name.toLowerCase().includes(searchText));
+        },
+        getLastAccess() {
+            const lastSentMessage = this.computedContact.messages
+                .filter(message => message.status === 'received')
+                .pop();
+            return lastSentMessage ? 'Ultimo accesso oggi alle' + this.getTimeFromDate(lastSentMessage.date) : 'Ultimo accesso nascosto';
         }
     },
     mounted() {
